4339ac2c5ad0dbee499ebfd8130f340b3ca3da86
robotman40 <robotman40@protonmail.com>
2026-05-17 19:17:50 -0700

    Implement initial definitions of weather

---
+++ src/weather_info.cpp
@@ -0,0 +1,75 @@
+#include "weather_info.hpp"
+#include "http_client.hpp"
+
+#include <string>
+#include <nlohmann/json.hpp>
+
+WeatherInfo::WeatherInfo(const std::string& url,
+                         const double latitude,
+                         const double longitude,
+                         HttpClient *client):
+    m_url(url + "points/" + std::to_string(latitude) + "," + std::to_string(longitude)),
+    m_client(client),
+    populated(false) {
+}
+
+WeatherFetchResult WeatherInfo::fetch() {
+    std::string response = m_client->get(m_url);
+    if (response.status_code / 100 == 3)
+        return WeatherFetchResult::DOESNT_EXIST;
+    else if (response.status_code != 200)
+        return WeatherFetchResult::INVALID_RESPONSE;
+
+    std::string hourlyForecastLink;
+    try {
+        hourlyForecastLink = getHourlyForecastLink(response);
+    } catch (const nlohmann::json::exception& e) {
+        return WeatherFetchResult::INVALID_RESPONSE;
+    }
+
+    std::string hourlyForecastResponse = m_client->get(hourlyForecastLink);
+    if (hourlyForecastResponse.status_code != 200)
+        return WeatherFetchResult::INVALID_RESPONSE;
+
+    try {
+        populateFromHourlyForecast(hourlyForecastResponse);
+    } catch (const nlohmann::json::exception& e) {
+        return WeatherFetchResult::INVALID_RESPONSE;
+    }
+
+    return WeatherFetchResult::OK;
+}
+
+void WeatherInfo::print(std::ostream &out) const {
+    if (!populated) return;
+    out << "Temperature: " << m_temperature << std::endl;
+    out << "Precipitation: " << m_precipitation << std::endl;
+    out << "Wind: " << m_wind << std::endl;
+    out << "Humidity: " << m_humidity << std::endl;
+}
+
+std::string WeatherInfo::getHourlyForecastLink(const std::string& response) {
+    nlohmann::json jsonResponse = nlohmann::json::parse(response);
+    return jsonResponse["properties"]["forecastHourly"]["href"];
+}
+
+void WeatherInfo::populateFromHourlyForecast(const std::string& hourlyForecastResponse) {
+    nlohmann::json hourlyForecastJson = nlohmann::json::parse(hourlyForecastResponse);
+
+    // Populate with the first item in the hourly forecast
+    if (hourlyForecastJson.empty()) {
+        throw std::runtime_error("Hourly forecast is empty");
+    }
+
+    try {
+        nlohmann::json firstItem = hourlyForecastJson["properties"]["periods"]["data"][0];
+        m_temperature = firstItem["temperature"] + " " + firstItem["temperatureUnit"];
+        m_precipitation = firstItem["probabilityOfPrecipitation"] + "%";
+        m_wind = firstItem["windSpeed"] + " " + firstItem["windDirection"];
+        m_humidity = firstItem["relativeHumidity"]["value"] + "%";
+    } catch (const nlohmann::json::exception& e) {
+        throw std::runtime_error("Failed to parse hourly forecast: " + std::string(e.what()));
+    }
+
+    populated = true;
+}
