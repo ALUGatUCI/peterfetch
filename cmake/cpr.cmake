b8d5a714b269637812e5ade3660394839309395d
Chris Rios <robotman40@protonmail.com>
2026-05-20 23:14:59 -0700

    Enable Win32-specific flags for compilation via MSVC

--- cmake/cpr.cmake
+++ cmake/cpr.cmake
@@ -1,6 +1,12 @@
 include(FetchContent)
 
-set(CPR_USE_SYSTEM_CURL TRUE)
+if (WIN32)
+    set(CPR_USE_SYSTEM_CURL OFF)
+     set(CPR_CURL_USE_LIBPSL OFF)
+else()
+    set(CPR_USE_SYSTEM_CURL ON)
+endif()
+
 FetchContent_Declare(
     cpr
     SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/3rd-party/cpr
